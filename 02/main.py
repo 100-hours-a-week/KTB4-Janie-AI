28e88fe0ad70aeaa915373b2b0d2405c2134b7ab
zlwony <jiwonwin4548@gmail.com>
2026-05-19 16:13:41 +0900

    02 서브모듈 제거

---
+++ 02/main.py
@@ -0,0 +1,34 @@
+from fastapi import FastAPI
+from pydantic import BaseModel
+
+app = FastAPI()
+
+class Post(BaseModel):
+    title: str
+    content: str
+    
+posts = list()
+
+@app.post('/posts')
+def create_post(post: Post):
+    post_id = len(posts)
+    posts.append({'id':post_id, 'title': post.title, 'content': post.content})
+    return {'post_id':post_id, 'message': 'Post created'}
+
+@app.get('/posts')
+def get_posts():
+    return posts
+
+@app.get('/posts/{post_id}')
+def get_post(post_id: int):
+    return posts[post_id]
+
+@app.put('/posts/{post_id}')
+def update_post(post_id: int, post: Post):
+    posts[post_id] = post
+    return post
+
+@app.delete('/posts/{post_id}')
+def delete_post(post_id: int):
+    posts.pop(post_id)
+    return {'message': 'Post deleted'}
